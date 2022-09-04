0781c23d67fc289300c126c7d3eea624f8a89f37
Viraj Hanmante <110629735+virajhanmante@users.noreply.github.com>
2022-09-04 16:50:23 +0530

    Add files via upload

--- automation.sh
+++ automation.sh
@@ -1,3 +1,39 @@
+sudo apt update -y
+aptvar=$(dpkg --get-selections apache2 |awk '{print $1}')
+if [ "$aptvar"="apache2" ]
+        then
+                echo "apache is already installed"
+else
+        sudo apt-get update
+        sudo apt install apache2
+        echo "apache is installed"
+fi
+activeptr=$(sudo systemctl status apache2 | grep "active" | awk '{print $2}')
+if [ "$activeptr"="active" ]
+        then
+                echo "apache is already active"
+else
+        sudo service apache2 start
+        echo "apache is active"
+fi
+serviceptr=$(sudo systemctl is-enabled apache2 | awk '{print $1}')
+if [ "$serviceptr"="enabled" ]
+        then
+                echo "apache service is already enabled"
+else
+        sudo systemctl enable apache2
+        echo "service is enabled"
+fi
+name='Viraj'
+timestamp=$(date '+%d%m%Y-%H%M%S')
+cd /var/log/apache2/
+tar -czvf /tmp/${name}-httpd-logs-${timestamp}.tar *.log
+
+sudo apt update
+sudo apt install awscli
+
+s3bucket='upgrad-viraj'
+
 aws s3 \
-cp /tmp/Viraj-httpd-logs-31082022-013947.tar \
-s3://upgrad-viraj/Viraj-httpd-logs-31082022-013947.tar
+cp /tmp/${name}-httpd-logs-${timestamp}.tar \
+s3://${s3bucket}/${name}-httpd-logs-${timestamp}.tar
